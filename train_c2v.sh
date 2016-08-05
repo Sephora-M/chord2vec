2b458891cb3e2b3653bcce94df6ecb3ea4c2a9e4
Sephora-M <sephora.madjiheurem@gmail.com>
2016-08-05 19:35:16 +1000

    tr

--- train_c2v.sh
+++ train_c2v.sh
@@ -3,12 +3,12 @@ BATCH_SIZE=128
 STEPS_CKPT=100
 MAX_EPOCHS=20
 #DATA_FILES='MuseData Nottingham'
-#DATA_FILES='Piano-midi.de JSB_Chorales'
-for DATA_FILE in $DATA_FILES
+DATA_FILES='JSB_Chorales'
+for NUM_UNITS in 512 1024
    do
-   for NUM_LAYER in 3 2
+   for NUM_LAYER in 3 2 1
    	do  
-   	python main.py --num_layers=$NUM_LAYER --num_units=1024  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir $DATA_FILE'_'$NUM_LAYER'layers_attention' --attention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle'
-   	python main.py --num_layers=$NUM_LAYER --num_units=1024  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir $DATA_FILE'_'$NUM_LAYER'layers' --noattention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle'
+   	python main.py --num_layers=$NUM_LAYER --num_units=1024  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir $DATA_FILE'_'$NUM_LAYER'_'$NUM_UNITS'layers_attention' --attention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle'
+   	python main.py --num_layers=$NUM_LAYER --num_units=1024  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir $DATA_FILE'_'$NUM_LAYER'_'$NUM_UNITS'layers' --noattention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle'
    done
 done
