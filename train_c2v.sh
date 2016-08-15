73331d7090546045179fc4b752e41cc3f6f2eda6
Christian Walder <walderchristian@gmail.com>
2016-08-15 16:39:02 +1000

    updated shell sript

--- train_c2v.sh
+++ train_c2v.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-BATCH_SIZE=128
-STEPS_CKPT=100
+STEPS_CKPT=200
 MAX_EPOCHS=20
 #DATA_FILES='MuseData Nottingham'
-DATA_FILES='JSB_Chorales'
+DATA_FILE='JSB_Chorales'
 for NUM_UNITS in 512 1024
-   do
-   for NUM_LAYER in 3 2 1
-   	do  
-   	python main.py --num_layers=$NUM_LAYER --num_units=1024  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir $DATA_FILE'_'$NUM_LAYER'_'$NUM_UNITS'layers_attention' --attention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle'
-   	python main.py --num_layers=$NUM_LAYER --num_units=1024  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir $DATA_FILE'_'$NUM_LAYER'_'$NUM_UNITS'layers' --noattention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle'
-   done
+do
+for NUM_LAYER in 2 1
+ do
+  for BATCH_SIZE in 64 128 
+   do  
+    python main.py --num_layers=$NUM_LAYER --num_units=$NUM_UNITS  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir 'save_models/'$NUM_LAYER'layers_models/GD/'$DATA_FILE'_'$BATCH_SIZE'batch_'$NUM_LAYER'layers_'$NUM_UNITS'units_attention' --attention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle' --GD --learning_rate=0.5
+    python main.py --num_layers=$NUM_LAYER --num_units=$NUM_UNITS  --max_epochs=$MAX_EPOCHS --batch_size=$BATCH_SIZE --train_dir 'save_models/'$NUM_LAYER'layers_models/GD/'$DATA_FILE'_'$BATCH_SIZE'batch_'$NUM_LAYER'layers_'$NUM_UNIT'units' --noattention --steps_per_checkpoint=$STEPS_CKPT --data_file $DATA_FILE'.pickle' --GD --learning_rate=0.5
+done
+done 
 done
