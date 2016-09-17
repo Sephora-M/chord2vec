4e7400072f580932fdcb78347abd344c96881be0
Sephora-M <sephora.madjiheurem@gmail.com>
2016-09-17 10:39:21 +0200

    removed unused files

--- train_nade_like.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-DATA_FILES='Nottingham Piano-midi.de.pickle all'
-for DATA_FILE in $DATA_FILES
-  do
-  python nade_like.py $DATA_FILE'.pickle' save_models/nade/'DATA_FILE'_nade_like.ckpt
-done
